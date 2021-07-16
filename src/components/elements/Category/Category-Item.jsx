18d013bff4c03c53996b25701c9a57ed0176917a
mathi931 <mesi.mathi@gmail.com>
2021-07-16 17:27:00 +0200

    directory on main page with hover effects

--- src/components/elements/Category/Category-Item.jsx
+++ src/components/elements/Category/Category-Item.jsx
@@ -4,11 +4,17 @@ import Title from './Category-Title';
 
 const Container = styled.div(({ size }) => [
 	tw`overflow-hidden bg-red-100 cursor-pointer shadow-sm rounded w-80 h-80 flex-grow max-w-lg flex items-center justify-center`,
-	size === 'large' && tw` md:max-w-full lg:h-96`
+	size === 'large' && tw` md:max-w-full lg:h-96`,
+	`	&:hover .css-gw656r{
+			opacity: 80%;
+	}`,
+	`		&:hover .css-1npee0z{
+		transform: scale(1.1);
+	}`
 ]);
 
 const BackGround = styled.div`
-	${tw`transform transition ease-in-out duration-700 hover:scale-110 w-full h-full bg-cover bg-center`}
+	${tw`transform transition ease-in-out duration-700 w-full h-full bg-cover bg-center`}
 `;
 
 const Category = ({ size, cTitle, Iurl }) => {
